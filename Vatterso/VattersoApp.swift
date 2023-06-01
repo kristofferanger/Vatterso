1d821e07d0db398a0de14f0177d6d6fb9fa93037
Kristoffer Anger <kristoffer.anger@gmail.com>
2023-06-01 10:08:16 +0200

    Initial Commit

---
+++ Vatterso/VattersoApp.swift
@@ -0,0 +1,20 @@
+//
+//  VattersoApp.swift
+//  Vatterso
+//
+//  Created by Kristoffer Anger on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct VattersoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
